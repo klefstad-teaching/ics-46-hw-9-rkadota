b8faca29dd0e04b3bfcd95061b00bcaa35b23051
Richie Kadota <rkadota@uci.edu>
2025-03-16 23:09:43 +0000

    po

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,14 +1,65 @@
 #include "ladder.h"
 
 void error(string word1, string word2, string msg) {
-
+    cout << "Error: " << msg << endl;
 }
+
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
     return true;
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
-    return true;
+    int word1_len = word1.length();
+    int word2_len = word2.length();
+    int dif = abs(word1_len - word2_len);
+
+    if (dif > 1) {
+        cout << "Diff great than 1" << endl;
+        return false;
+    }
+
+    if (dif == 0) {
+        int count = 0;
+        int differences = 0;
+        for (auto character: word1) {
+            if (character != word2[count]) {
+                ++differences;
+            }
+            if (differences > 1) {return false;}
+            ++count;
+        }
+
+        return true;
+    }
+
+    else {
+        string longest;
+        string shortest;
+        if (word1_len > word2_len) {
+            longest = word1;
+            shortest = word2;
+        }
+        else {
+            longest = word2;
+            shortest = word1;
+        }
+
+        int count = 0;
+        int differences = 0;
+        for (auto character: shortest) {
+            if (longest[count] != character) {
+                ++differences;
+                //cout << longest[count] << " " << character;
+                }
+            if (differences > 1) {return false;}
+            ++count;
+        }
+        
+        if (differences == 0) {
+            return true;
+        }
+        else {return false;}
+    }
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
@@ -17,10 +68,17 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
 }
 
 void load_words(set<string> & word_list, const string& file_name) {
-
+    ifstream in(file_name);
+    for (string word; in >> word;) {
+        word_list.insert(word);
+    }
 }
-void print_word_ladder(const vector<string>& ladder) {
 
+void print_word_ladder(const vector<string>& ladder) {
+    for (auto word: ladder) {
+        cout << word << " ";
+    }
+    cout << endl;
 }
 
 void verify_word_ladder() {
--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -1,3 +1,13 @@
+#include "ladder.h"
+
+void test_is_adjacent() {
+    cout << is_adjacent("Pog", "pog") << endl;
+    cout << is_adjacent("Fwg", "pog") << endl;
+    cout << is_adjacent("Dog", "Dogg") << endl;
+}
+
+
 int main() {
+    test_is_adjacent();
     return 0;
 }
