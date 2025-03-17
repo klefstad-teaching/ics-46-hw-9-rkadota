62787fe8fdcdf6f1a7616310d6bd703c854c24ad
Richie Kadota <rkadota@uci.edu>
2025-03-17 00:03:59 +0000

    hello.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -5,7 +5,7 @@ void error(string word1, string word2, string msg) {
 }
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
-    return is_adjacent(str1, str2);
+    return true;
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
